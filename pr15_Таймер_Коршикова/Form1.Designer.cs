d3b3f091d4c15353d96cd0236218a943242536b3
Эльвина Коршикова <106970196+elvina27@users.noreply.github.com>
2023-02-06 23:58:39 +0300

    Add files via upload

---
+++ pr15_Таймер_Коршикова/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace pr15_Таймер_Коршикова
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.SbrosButton = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Green;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(525, 135);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Olive;
+            this.label2.Location = new System.Drawing.Point(157, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "дата";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Green;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(96, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(272, 113);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0:0.0";
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(131, 321);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Стоп";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // SbrosButton
+            // 
+            this.SbrosButton.Location = new System.Drawing.Point(257, 321);
+            this.SbrosButton.Name = "SbrosButton";
+            this.SbrosButton.Size = new System.Drawing.Size(75, 23);
+            this.SbrosButton.TabIndex = 4;
+            this.SbrosButton.Text = "Сброс";
+            this.SbrosButton.UseVisualStyleBackColor = true;
+            this.SbrosButton.Click += new System.EventHandler(this.SbrosButton_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 356);
+            this.Controls.Add(this.SbrosButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Дата и время";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button SbrosButton;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ pr15_Таймер_Коршикова/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr15_Таймер_Коршикова
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongTimeString();
+            label2.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private int m, s, ms;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            ms++;
+            s = ms / 10;
+            m = s / 60;
+            label3.Text = m + ":" + s % 50 + "." + ms % 10;
+        }
+
+        private void SbrosButton_Click(object sender, EventArgs e)
+        {
+            m = 0; s = 0; ms = 0;
+            label3.Text = "0:0.0";
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            if(timer2.Enabled)
+            {
+                timer2.Stop();
+                StopButton.Text = "Старт";
+            }
+            else
+            {
+                timer2.Start();
+                StopButton.Text = "Стоп";
+            }
+        }
+    }
+}
